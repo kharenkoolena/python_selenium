61308d64cfb47244668397e39414a48ba56a1805
kharenkoolena <kharenko.olena@gmail.com>
2023-11-04 18:00:52 +0100

    Implementation of Firefox support

--- tests/test_base.py
+++ tests/test_base.py
@@ -3,17 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-# @pytest.fixture(scope="module")
-# def setup():
-#     print("Running BEFORE")
-#     o = Options()
-#     o.add_experimental_option("detach", True)
-#     driver = webdriver.Chrome(options=o)
-#     driver.implicitly_wait(10)
-#     driver.get("https:/leetcode.com/accounts/login/")
-#     yield driver
-#     print("Running AFTER")
-#     driver.quit()
 @pytest.fixture(scope="module")
 def setup(request):
     browser_name = request.config.getoption("--browser")
