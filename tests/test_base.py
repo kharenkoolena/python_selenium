1d3e6acca4dd27cef3ec0db899b90709dbfd0242
kharenkoolena <kharenko.olena@gmail.com>
2023-11-06 17:17:35 +0100

    Chrome headless support implementation

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,14 +1,24 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 
 @pytest.fixture(scope="module")
 def setup(request):
     browser_name = request.config.getoption("--browser")
     print("Running BEFORE")
+    # chrome_options = ChromeOptions()
+    # chrome_options.add_argument("--headless")
+    # chrome_options.add_experimental_option("detach", True)
+    # driver = webdriver.Chrome(options=chrome_options)
     if browser_name == "firefox":
         driver = webdriver.Firefox()
+    elif browser_name == "chrome--headless":
+        chrome_options = ChromeOptions()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_experimental_option("detach", True)
+        driver = webdriver.Chrome(options=chrome_options)
     else:
         o = Options()
         o.add_experimental_option("detach", True)
